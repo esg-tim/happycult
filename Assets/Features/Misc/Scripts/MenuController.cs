49ff35f23fb15e4fa07fda3206ca2512d09aa647
Tim Mann <tdmann@gmail.com>
2016-01-31 03:54:13 -0800

    Adding main menu.

---
+++ Assets/Features/Misc/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour 
+{
+	[SerializeField]
+	private CanvasGroup canvasGroup;
+
+	private Coroutine startGameCoroutine;
+	public void OnPlayPressed()
+	{
+		if (startGameCoroutine == null)
+		{
+			startGameCoroutine = StartCoroutine(StartGame());
+		}
+	}
+
+	private IEnumerator StartGame()
+	{
+		var t = 0f;
+		while (t < 1)
+		{
+			canvasGroup.alpha = 1.0f - t;
+			yield return new WaitForEndOfFrame();
+			t += Time.deltaTime;
+		}
+
+		SceneManager.LoadScene("Main");
+	}
+}
